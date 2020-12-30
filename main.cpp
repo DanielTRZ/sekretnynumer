b0f764f02d69ebed1ad5b76890290406ce355fee
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2020-12-30 15:57:32 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    string haslo;
+
+    cout<<"Zgadnij sekretny numer"<<endl;
+
+    cout<< "Podaj liczbe calkowita: ";
+
+
+    while(cin>>haslo)
+    {
+
+        if(haslo != "3")
+        {
+            cout<< "Nie, to nie jest ta liczba, ktora wybralem dla ciebie. Sprobuj ponownie!. " <<endl;
+
+            cout<< "Podaj haslo: ";
+        }
+
+        else
+        {
+            cout<< "Dobra robota! To numer, ktory wybralem dla ciebie! Jestes teraz wolny."<<endl;
+            exit(0);
+        }
+    }
+
+    return 0;
+}
